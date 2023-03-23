de86271516b1a8ccea7227a668c1b8638a4ef7b6
LEGION <omarjafarli2009@gmail.com>
2023-03-23 23:26:15 +0400

    internal commit

--- 4.py
+++ 4.py
@@ -1,4 +1,18 @@
-import random
-import math
+class NumberGenerator:
+    def __init__(self, start, stop):
+        self.start = start
+        self.stop = stop
 
-vashechislo =  print(int(input("Введите число:")))
+    def __iter__(self):
+        self.current = self.start
+        return self
+
+    def __next__(self):
+        if self.current >= self.stop:
+            raise StopIteration
+        else:
+            self.current += 1
+            return self.current - 1
+generator = NumberGenerator(1, 5)
+for num in generator:
+    print(num)
